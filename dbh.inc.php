302c5c6c177b75fca6f201c9f01fc77df633fecf
h54023kc <ka.chau-3@student.manchester.ac.uk>
2022-02-24 18:43:13 +0000

    DB test

---
+++ dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+$database_host = "dbhost.cs.man.ac.uk";
+$database_user = "h54023kc";
+$database_pass = "y20-pass";
+$group_dbnames = "2021_comp10120_y20";
+
+$conn = mysqli_connect($database_host,$database_user,$database_pass,$group_dbnames);
+
+?>
