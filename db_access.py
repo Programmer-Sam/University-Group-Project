530944cac16a50e024ece85c8cc5bc7ab1821895
h54023kc <ka.chau-3@student.manchester.ac.uk>
2022-03-24 03:17:56 +0000

    added more db access

--- db_access.py
+++ db_access.py
@@ -29,10 +29,10 @@ class restaurants(db.Model):
         self.Linkweb = Linkweb
 
 class LIKED(db.Model):
-    id = db.Column('user_id', db.Integer, primary_key = True)
+    id = db.Column('id', db.Integer, primary_key = True)
     Disliked_bit = db.Column(db.Boolean)
     Userid_int = db.Column(db.Integer)
-    Restaurantid_int = db.Column(db.Boolean)
+    Restaurantid_int = db.Column(db.Integer)
 
     def __init__(self,Disliked_bit,Userid_int,Restaurantid_int):
         self.Disliked_bit = Disliked_bit
@@ -42,13 +42,14 @@ def create_db():
     db.create_all()
 
 def AddRestaurants():
-    name = "Pizza Hut"
-    food_type = "Pizza"
+    name = "Ice Stone Gelato"
+    food_type = "Ice Cream and Deserts"
     Address = "M1 7DY"
-    Linkweb = "https://www.google.com/maps/place/Pizza+Hut+Delivery/@53.4704223,-2.2373532,21z/data=!4m13!1m7!3m6!1s0x487bb19331bc9ed3:0x5a905cb68ffc970b!2sOxford+Rd,+Manchester+M1+7DY!3b1!8m2!3d53.4704789!4d-2.2370621!3m4!1s0x487bb19331a1a057:0x790f603af1c067c7!8m2!3d53.4704108!4d-2.2371241"
+    Linkweb = "https://www.google.com/maps/place/IceStone+Gelato/@53.4704223,-2.2373532,21z/data=!4m13!1m7!3m6!1s0x487bb19331bc9ed3:0x5a905cb68ffc970b!2sOxford+Rd,+Manchester+M1+7DY!3b1!8m2!3d53.4704789!4d-2.2370621!3m4!1s0x487bb19331f7e379:0x105d79e57595fd10!8m2!3d53.4704983!4d-2.2372026"
     inserted_row = restaurants(name,food_type,Address,"",Linkweb)
     db.session.add (inserted_row)
     db.session.commit()
+
 def insert_db(email, password):
     inserted_row = users(email,password)
     db.session.add (inserted_row)
@@ -74,4 +75,22 @@ def getRestaurants():
                             record.Location,
                             record.Linkpic,
                             record.Linkweb])
+    return output_data
+
+def getRestaurantsByID(id):
+    data = restaurants.query.filter_by(id=id).first()
+    return [data.Restaurant_Name, data.Food_Type, data.Location, data.Linkpic, data.Linkweb]
+
+def addLikedLink(Disliked_bit, Userid_int, Restaurantid_int):
+    inserted_row = LIKED(Disliked_bit, Userid_int, Restaurantid_int)
+    db.session.add (inserted_row)
+    db.session.commit()
+
+def getLikesByUserID(user_id):
+    data = LIKED.query.filter_by(Userid_int=user_id).all()
+    output_data = []
+    for index, record in enumerate(data):
+        output_data.append([record.Disliked_bit,
+                            record.Restaurantid_int])
+        output_data[index].append(getRestaurantsByID(record.Restaurantid_int))
     return output_data
